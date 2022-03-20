44fde632bbd7f3500d2f7b835e1130c99e06bd45
Luke Billard <lukebillard04@gmail.com>
2022-03-19 23:12:28 -0230

    Initial commit for new task feature

--- TaskMaster/src/main/java/com/taskmaster/todo/TodoController.java
+++ TaskMaster/src/main/java/com/taskmaster/todo/TodoController.java
@@ -1,13 +1,26 @@
 package com.taskmaster.todo;
 
 import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class TodoController {
-	@GetMapping("/Todo")
-	public String getTodoView()
+	@GetMapping("/todo")
+	public String getTodoView(ModelMap model)
 	{
 		return "todo";
 	}
+	
+	@GetMapping("/todo/newtask")
+	public String getTodoNewtaskView(ModelMap model)
+	{
+		return "newtask";
+	}
+	
+	
+	
+	
 }
