87f9d93cdd08aa403c5a217ec08c1e1e5ea1effe
Luke Billard <lukebillard04@gmail.com>
2022-03-21 16:46:26 -0230

    Added Setter for ArrayList of tasks in TodoDto

--- TaskMaster/src/main/java/com/taskmaster/todo/TodoDto.java
+++ TaskMaster/src/main/java/com/taskmaster/todo/TodoDto.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 public class TodoDto {
 private ArrayList<Task> tasks;
 
+
+public void setTasks(ArrayList<Task> tasks) {
+	this.tasks = tasks;
+}
+
 public ArrayList<Task> getTasks() {
 	return tasks;
 }
