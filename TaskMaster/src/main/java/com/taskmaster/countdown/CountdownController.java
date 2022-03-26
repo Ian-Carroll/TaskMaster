bad4fa9da618d9f701f5bdf3a7bf6e83f973b885
Ian Carroll <ianpatcarr@gmail.com>
2022-03-26 21:22:11 -0230

    Fixed CountdownController to accept form Posts.

--- TaskMaster/src/main/java/com/taskmaster/countdown/CountdownController.java
+++ TaskMaster/src/main/java/com/taskmaster/countdown/CountdownController.java
@@ -3,6 +3,8 @@ package com.taskmaster.countdown;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CountdownController
@@ -14,15 +16,25 @@ public class CountdownController
 		{
 			countdownDto.setState("Work");
 		}
+		countdownDto.setSessionTimeSecs(0);
 		model.put("state", "Break");
 		model.put("display", "none");
 		return "countdown";
 	}
 	
 	@GetMapping("/config_countdown")
-	public String getConfigCountdownView(ModelMap model, CountdownDto countdownDto)
+	public String getConfigCountdownView(ModelMap model)
 	{
+		model.put("countdownDto", new CountdownDto());
 		return "config_countdown";
 	}
-
+	
+	@PostMapping("/config_countdown")
+	public String configCountdownSubmit(ModelMap model, @ModelAttribute CountdownDto countdownDto)
+	{
+		countdownDto.setState("Work");
+		model.put("countdownDto", countdownDto);
+		return "countdown";
+	}
+	
 }
