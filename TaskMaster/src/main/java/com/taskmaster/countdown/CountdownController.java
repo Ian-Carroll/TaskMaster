79c2ee1d6a9e4dade4208c34edaefd413a0293d0
Ian Carroll <ianpatcarr@gmail.com>
2022-04-04 20:23:17 -0230

    Added styling and args to mapping subroutines to prevent data loss.

--- TaskMaster/src/main/java/com/taskmaster/countdown/CountdownController.java
+++ TaskMaster/src/main/java/com/taskmaster/countdown/CountdownController.java
@@ -6,9 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+// CountdownController handles all HTTP GET and POST
+// mapping events (i.e. get the website page, or post data through HTML forms)
+// from the front-end.
 @Controller
 public class CountdownController
 {
+	// Returns the countdown.html template on get request
 	@GetMapping("/")
 	public String getCountdownView (ModelMap model, CountdownDto countdownDto)
 	{
@@ -17,18 +21,32 @@ public class CountdownController
 			countdownDto.setState("Work");
 		}
 		countdownDto.setSessionTimeSecs(0);
-		model.put("state", "Break");
-		model.put("display", "none");
+		model.put("countdownDto", countdownDto);
 		return "countdown";
 	}
 	
-	@GetMapping("/config_countdown")
-	public String getConfigCountdownView(ModelMap model)
+	// Submits the CountdownDto data, transferring user from countdown page
+	// to config_countdown page
+	@PostMapping("/")
+	public String coundownConfigSubmit(ModelMap model, CountdownDto countdownDto)
 	{
-		model.put("countdownDto", new CountdownDto());
+		countdownDto.setState("Work");
+		countdownDto.setSessionTimeSecs(0);
 		return "config_countdown";
 	}
 	
+	// Returns the config_countdown.html template on get request
+	@GetMapping("/config_countdown")
+	public String getConfigCountdownView(ModelMap model, CountdownDto countdownDto)
+	{
+		countdownDto.setState("Work");
+		countdownDto.setSessionTimeSecs(0);
+		model.put("countdownDto", countdownDto);
+		return "config_countdown";
+	}
+	
+	// Submits the CountdownDto data, transferring user from config_countdown page
+	// to countdown page
 	@PostMapping("/config_countdown")
 	public String configCountdownSubmit(ModelMap model, @ModelAttribute CountdownDto countdownDto)
 	{
