bdb89de86ede3c183d24a7e8a532805153b09cc6
Ian Carroll <ianpatcarr@gmail.com>
2022-04-04 20:21:38 -0230

    Added comments and styling.

--- TaskMaster/src/main/resources/static/js/config_countdown.js
+++ TaskMaster/src/main/resources/static/js/config_countdown.js
@@ -1,30 +1,40 @@
 // Make all forms readonly
 function setFormsReadOnly()
 {
-	document.getElementById("sessionTimeHrs").setAttribute("readonly", "true")
-	document.getElementById("sessionTimeMins").setAttribute("readonly", "true")
-	document.getElementById("numBreaks").setAttribute("readonly", "true")
-	document.getElementById("breakLength").setAttribute("readonly", "true")
+	document.getElementById("sessionTimeHrs").setAttribute("readonly", "true");
+	document.getElementById("sessionTimeMins").setAttribute("readonly", "true");
+	document.getElementById("numBreaks").setAttribute("readonly", "true");
+	document.getElementById("breakLength").setAttribute("readonly", "true");
 }
 
 // Set input forms back to enable writing
 function setFormsWrite()
 {
-	document.getElementById("sessionTimeHrs").removeAttribute("readonly")
-	document.getElementById("sessionTimeMins").removeAttribute("readonly")
-	document.getElementById("numBreaks").removeAttribute("readonly")
-	document.getElementById("breakLength").removeAttribute("readonly")
+	document.getElementById("sessionTimeHrs").removeAttribute("readonly");
+	document.getElementById("sessionTimeMins").removeAttribute("readonly");
+	document.getElementById("numBreaks").removeAttribute("readonly");
+	document.getElementById("breakLength").removeAttribute("readonly");
 }
 
 // Set all input forms to 0
 function clearAllInputs()
 {
-	document.getElementById('sessionTimeHrs').value = 0
-	document.getElementById('sessionTimeMins').value = 0
-	document.getElementById('numBreaks').value = 0
-	document.getElementById('breakLength').value = 0
+	document.getElementById('sessionTimeHrs').value = 0;
+	document.getElementById('sessionTimeMins').value = 0;
+	document.getElementById('numBreaks').value = 0;
+	document.getElementById('breakLength').value = 0;
 }
 
+// On startup, make sure boxes are readonly 0's if a preset is selected
+var isPomodoro = document.getElementById('isPomodoro').checked;
+var isShortPeriod = document.getElementById('isShortPeriod').checked;
+var isLongPeriod = document.getElementById('isLongPeriod').checked;
+
+if (isPomodoro || isShortPeriod || isLongPeriod)
+{
+	clearAllInputs();
+	setFormsReadOnly();
+}
 
 // If the Pomodoro checkbox gets clicked
 document.getElementById('isPomodoro').addEventListener('click', function()
@@ -32,19 +42,19 @@ document.getElementById('isPomodoro').addEventListener('click', function()
 	// If the checkbox is checked, set all forms to readonly
 	if (document.getElementById('isPomodoro').checked == true)
 	{
-		setFormsReadOnly()
+		setFormsReadOnly();
 	}
 	// Otherwise enable writing to inputs
 	else
 	{
-		setFormsWrite()
+		setFormsWrite();
 	}
 	
 	// Clear the other preset default checkboxes
-	document.getElementById('isShortPeriod').checked = false
-	document.getElementById('isLongPeriod').checked = false
+	document.getElementById('isShortPeriod').checked = false;
+	document.getElementById('isLongPeriod').checked = false;
 	
-	clearAllInputs()
+	clearAllInputs();
 })
 
 // If the ShortPeriod checkbox gets clicked
@@ -53,19 +63,19 @@ document.getElementById('isShortPeriod').addEventListener('click', function()
 	// If the checkbox is checked, set all forms to readonly
 	if (document.getElementById('isShortPeriod').checked == true)
 	{
-		setFormsReadOnly()
+		setFormsReadOnly();
 	}
 	// Otherwise enable writing to inputs
 	else
 	{
-		setFormsWrite()
+		setFormsWrite();
 	}
 	
 	// Clear the other preset default checkboxes
-	document.getElementById('isPomodoro').checked = false
-	document.getElementById('isLongPeriod').checked = false
+	document.getElementById('isPomodoro').checked = false;
+	document.getElementById('isLongPeriod').checked = false;
 	
-	clearAllInputs()
+	clearAllInputs();
 })
 
 // If the LongPeriod checkbox gets clicked
@@ -74,24 +84,17 @@ document.getElementById('isLongPeriod').addEventListener('click', function()
 	// If the checkbox is checked, set all forms to readonly
 	if (document.getElementById('isLongPeriod').checked == true)
 	{
-		setFormsReadOnly()
+		setFormsReadOnly();
 	}
 	// Otherwise enable writing to inputs
 	else
 	{
-		setFormsWrite()
+		setFormsWrite();
 	}
 	
 	// Clear the other preset default checkboxes
-	document.getElementById('isPomodoro').checked = false
-	document.getElementById('isShortPeriod').checked = false
+	document.getElementById('isPomodoro').checked = false;
+	document.getElementById('isShortPeriod').checked = false;
 	
-	clearAllInputs()
-})
-
-// If the submit button gets clicked
-document.getElementById('submit').addEventListener('click', function()
-{
-	sessionStorage.setItem('isPomodoro', document.getElementById('isPomodoro').checked)
-	sessionStorage.setItem('isShortPeriod', document.getElementById('isShortPeriod').checked)
-})
+	clearAllInputs();
+});
