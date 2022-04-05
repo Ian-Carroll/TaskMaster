c7a5a1a153e63afa492d0cfa644af3f1a6ba7ef7
Ian Carroll <ianpatcarr@gmail.com>
2022-04-05 11:33:53 -0230

    Revert to old commit.

--- TaskMaster/src/main/java/com/taskmaster/todo/TodoController.java
+++ TaskMaster/src/main/java/com/taskmaster/todo/TodoController.java
@@ -1,9 +1,6 @@
 package com.taskmaster.todo;
 
-import java.util.ArrayList;
-
 import org.springframework.stereotype.Controller;
-
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,40 +8,40 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class TodoController {
-	
-	@GetMapping("/")
+	@GetMapping("/todo")
 	public String getTodoView(ModelMap model, @ModelAttribute Task task, @ModelAttribute TodoDto todoDto)
 	{
-		if (todoDto.getTasks().size() == 0)
-		{
-			Task placeholder = new Task();
-			placeholder.setName("placeholder");
-			todoDto.addTask(placeholder);
-		}
+		//if (todoDto.getTasks() == null)
+		//{
+			//todoDto.addTask(task);
+		//}
+		// If you comment these lines out the todo list is blank when you enter the page
+		//todoDto.addTask(task);
 		todoDto.defaultTask();
 		model.put("todoDto", todoDto);
-		model.put("taskArray", todoDto.getTasks());
 		return "todo";
 	}
-	
-	@PostMapping("/")
-	public String TodoSubmit(ModelMap model, @ModelAttribute("todoDto") TodoDto todoDto)
+	@PostMapping("/todo")
+	public String TodoSubmit(ModelMap model, @ModelAttribute Task task, @ModelAttribute TodoDto todoDto)
 	{
-		//ArrayList<Task> tasks = todoDto.getTasks();
 		model.put("todoDto", todoDto);
 		return "newtask";
 	}
-	
 	@GetMapping("/todo/newtask")
 	public String getTodoNewtaskView(ModelMap model, @ModelAttribute Task task, @ModelAttribute TodoDto todoDto)
 	{
-		model.put("task", new Task());
+		//model.put("task", new Task());
+		//todoDto.addTask(task);
+		//todoDto.setTasks(todoDto.getTasks());
+		model.put("task", task);
 		model.put("todoDto",todoDto);
 		return "newtask";
 	}	
-	
 	@PostMapping("/todo/newtask")
 	public String NewTaskSubmit(ModelMap model, @ModelAttribute Task task, @ModelAttribute TodoDto todoDto) {
+		//task.setName(task.getName());
+		//task.setComment(task.getComment());
+		//todoDto.setTasks(todoDto.getTasks());
 		todoDto.addTask(task);
 		model.put("task",task);
 		model.put("todoDto",todoDto);
